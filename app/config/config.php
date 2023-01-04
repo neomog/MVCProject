01eaa5bc60673bf9887eabe32644da020fd4fa65
Ohiare Nathaniel <ohiarenathanielmog@gmail.com>
2023-01-05 00:50:50 +0100

    feat: add config file for constant definition

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * App Root Definition
+ */
+define('APPROOT', dirname(__FILE__, 2));
+/**
+ * URL Root Definition
+ */
+define('URLROOT', 'https://localhost/nathmvc');
+/**
+ * App Root Definition
+ */
+define('SITENAME', 'NathMVC');
---
+++ app/views/pages/index.php
@@ -0,0 +1,5 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+
+<h2><?= $data['title']; ?></h2>
+
+<?php require APPROOT . '/views/inc/footer.php'; ?>
